1da00fa9f40704ee2accdf7e8983f0119457f7b4
DhiaMnasser <mohameddhia.mnasser@esprit.tn>
2021-04-29 14:59:32 +0100

    fixed static in server/index.js

--- server/index.js
+++ server/index.js
@@ -213,7 +213,7 @@ const CONNECTION_URL = process.env.MONGODB_URL || 'mongodb+srv://admin:admin123@
 
 if(process.env.NODE_ENV ==="production"){
 
-    app.use(express.static("../client/build"));
+    app.use(express.static("client/build"));
 
     app.get("*", (req,res)=>{
         res.sendFile(path.resolve(__dirname, "../client", "build", index.html));
